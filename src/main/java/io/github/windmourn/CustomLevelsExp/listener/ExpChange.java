2c4f7c7a83638aa6028fc6b6fae458e6720102d0
a08381 <632785425@163.com>
2018-12-13 20:39:50 +0800

    .

--- src/main/java/io/github/windmourn/CustomLevelsExp/listener/ExpChange.java
+++ src/main/java/io/github/windmourn/CustomLevelsExp/listener/ExpChange.java
@@ -3,12 +3,13 @@ package io.github.windmourn.CustomLevelsExp.listener;
 import io.github.windmourn.CustomLevelsExp.exp.CustomExp;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerExpChangeEvent;
 
 public class ExpChange implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR)
     public void onExpChange(PlayerExpChangeEvent event) {
         int exp = event.getAmount();
         Player player = event.getPlayer();
